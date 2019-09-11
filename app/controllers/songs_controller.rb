786f7ac9d957c5273a6950b0caf56942a225240a
Solomon Feldman <solomonrfeldman@gmail.com>
2019-09-11 14:24:30 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,6 +1,6 @@
 class SongsController < ApplicationController
   def index
-    if params[:artost_id]
+    if params[:artist_id]
       artist = Artist.find_by(id: params[:artist_id])
       unless artist
         redirect_to artists_path, alert: "Artist not found"
